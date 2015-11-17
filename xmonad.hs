d538cca611b8203c0e745d739b001d77f192cfb6
Chris Len <cmlen23@gmail.com>
2015-11-17 22:56:45 +0000

    Initial commit.

---
+++ xmonad.hs
@@ -0,0 +1,42 @@
+import XMonad
+import XMonad.Layout.Spacing
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Hooks.DynamicLog  
+import XMonad.Hooks.ManageDocks  
+import XMonad.Util.Run  
+import System.IO
+
+   
+myLayouts = tiled ||| Mirror tiled ||| Full  
+	where  
+    	-- default tiling algorithm partitions the screen into two panes  
+     	tiled = Tall nmaster delta ratio  
+  
+ 	-- The default number of windows in the master pane  
+     	nmaster = 1  
+  
+     	-- Default proportion of screen occupied by master pane  
+     	ratio = 2/3  
+  
+     	-- Percent of screen to increment by when resizing panes  
+     	delta = 5/100 
+
+-- Define amount and names of workspaces
+myWorkspaces = ["1:main","2:chat","3","4","5","6","7","8","9"]
+
+main = 	do
+	xmproc <- spawnPipe "/usr/bin/xmobar /home/clen/.xmobarrc"
+	xmonad $ defaultConfig
+		{ borderWidth = 1  
+		,focusedBorderColor = "#ff0000" -- Red
+		, manageHook = manageDocks <+> manageHook defaultConfig
+		, layoutHook = avoidStruts $ layoutHook defaultConfig
+		, workspaces = myWorkspaces
+		, logHook = dynamicLogWithPP xmobarPP
+			{ ppOutput = hPutStrLn xmproc
+			, ppTitle = xmobarColor "blue" "" . shorten 50
+			, ppLayout = const "" -- to disable the layouy info on xmobar
+			}
+		}
+    
